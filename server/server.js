aa29c94619334bb74abd24dcacd19f0c2b1d2baf
IgnacioSoler1 <isoler@correo.um.edu.uy>
2023-09-20 09:12:12 -0300

    anda pasar email y password de font a back

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const usuariosRoutes = require('./scr/usuarios/routes');
 const app = express();
+const bodyParser = require("body-parser");
 const port = process.env.PORT || 300;
 
 // Importa el controlador de tareas
@@ -12,6 +13,22 @@ app.use('/api/usuarios', usuariosRoutes); //
 app.get("/api", (req,res) =>{
   res.json({"users": ["usuario1", "usuario2", "usuario3"]})
 })
+
+app.use(bodyParser.json());
+app.post("/api/login", (req, res) => {
+  const { email, password } = req.body;
+
+  // Aquí puedes realizar la autenticación del usuario y responder en consecuencia
+  // Por ejemplo, verificar las credenciales y devolver un token de acceso si son válidas.
+
+  if (email === "ejemplo2@email.com" && password === "contrasena123") {
+    res.status(200).json({ message: "Inicio de sesión exitoso" });
+    console.log("anduvo")
+  } else {
+    res.status(401).json({ message: "Credenciales incorrectas" });
+    console.log("no anduvo")
+  }
+});
 
 // Middleware para permitir el uso de JSON en las solicitudes
 app.use(express.json());
